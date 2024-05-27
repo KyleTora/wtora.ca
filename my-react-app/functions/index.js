cf79d9a05e5807a0f1f803c3543306c6e1c9ea84
Kyle Tora <kyletora1@gmail.com>
2024-05-27 14:35:22 -0400

    idl

--- my-react-app/functions/index.js
+++ my-react-app/functions/index.js
@@ -1,19 +1,54 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// functions/index.js
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const cors = require('cors')({ origin: true }); // Enable CORS for all origins
+
+admin.initializeApp();
+
+
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+});
+
+console.log(gmailEmail);
+
+exports.sendEmail = functions.https.onRequest(async (req, res) => {
+  cors(req, res, async () => {
+  
+    if (req.method !== 'POST') {
+        return res.status(400).send('Only POST requests are allowed');
+    }
+
+    const { name, email, message } = req.body;
+
+    const mailOptions = {
+        from: gmailEmail,
+        to: 'kyletora1@gmail.com',
+        subject: 'New Message from Contact Form',
+        text: `
+        Name: ${name}\n
+        Email: ${email}\n
+        Message: ${message}
+        `
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        return res.status(200).send('Email sent successfully');
+    } catch (error) {
+        console.error('Error sending email:', error);
+        return res.status(500).send('Failed to send email');
+    }
+  });
+});
